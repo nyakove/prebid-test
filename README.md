88d5f8c7785b449af3c38bd58d2bafa35305b599
admin3 <admin3@unity.local>
2019-02-06 14:08:57 +0200

    fix 'no module.js' file

---
+++ README.md
@@ -0,0 +1,13 @@
+**Install:**
+
+    git clone https://github.com/nyakove/prebit-test.git
+
+    cd prebit-test
+
+    npm install
+
+**Use:**
+
+    node index
+
+then go to http://127.0.0.1:7000/ and config prebid.js file
--- gulpHelpers.js
+++ gulpHelpers.js
@@ -1,5 +1,5 @@
 // this will have all of a copy of the normal fs methods as well
-const fs = require('fs.extra');
+const fs = require('fs-extra');
 const path = require('path');
 const argv = require('yargs').argv;
 const MANIFEST = 'package.json';
@@ -14,91 +14,90 @@ const DEV_PATH = './build/dev';
 
 
 function isModuleDirectory(filePath) {
-  try {
-    const manifestPath = path.join(filePath, MANIFEST);
-    if (fs.statSync(manifestPath).isFile()) {
-      const module = require(manifestPath);
-      return module && module.main;
-    }
-  }
-  catch (error) {}
+    try {
+        const manifestPath = path.join(filePath, MANIFEST);
+        if (fs.statSync(manifestPath).isFile()) {
+            const module = require(manifestPath);
+            return module && module.main;
+        }
+    } catch (error) {}
 }
 
 module.exports = {
-  toCapitalCase: function (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  },
+    toCapitalCase: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    },
 
-  jsonifyHTML: function (str) {
-    console.log(arguments);
-    return str.replace(/\n/g, '')
-        .replace(/<\//g, '<\\/')
-        .replace(/\/>/g, '\\/>');
-  },
-  getArgModules() {
-    var moduleFile = 'modules.json';
-      
-    var modules = JSON.parse(fs.readFileSync(moduleFile, 'utf8'));
+    jsonifyHTML: function (str) {
+        console.log(arguments);
+        return str.replace(/\n/g, '')
+            .replace(/<\//g, '<\\/')
+            .replace(/\/>/g, '\\/>');
+    },
+    getArgModules() {
+        let moduleFile = 'modules.json';
 
-    return modules;
-  },
-  getModules: _.memoize(function(externalModules) {
-    externalModules = externalModules || [];
-    var internalModules;
-    try {
-      var absoluteModulePath = path.join(__dirname, MODULE_PATH);
-      internalModules = fs.readdirSync(absoluteModulePath)
-        .filter(file => (/^[^\.]+(\.js)?$/).test(file))
-        .reduce((memo, file) => {
-          var moduleName = file.split(new RegExp('[.\\' + path.sep + ']'))[0];
-          var modulePath = path.join(absoluteModulePath, file);
-          if (fs.lstatSync(modulePath).isDirectory()) {
-            modulePath = path.join(modulePath, "index.js")
-          }
-          memo[modulePath] = moduleName;
-          return memo;
-        }, {});
-    } catch(err) {
-      internalModules = {};
-    }
-    return Object.assign(externalModules.reduce((memo, module) => {
-      try {
-        var modulePath = require.resolve(module);
-        memo[modulePath] = module;
-      } catch(err) {
-        // do something
-      }
-      return memo;
-    }, internalModules));
-  }),
+        let modules = JSON.parse(fs.readFileSync(moduleFile, 'utf8'));
 
-  getBuiltModules: function(dev, externalModules) {
-    var modules = this.getModuleNames(externalModules);
-    if(Array.isArray(externalModules)) {
-      modules = _.intersection(modules, externalModules);
-    }
-    return modules.map(name => path.join(__dirname, dev ? DEV_PATH : BUILD_PATH, name + '.js'));
-  },
+        return modules;
+    },
+    getModules: _.memoize(function (externalModules) {
+        externalModules = externalModules || [];
+        var internalModules;
+        try {
+            var absoluteModulePath = path.join(__dirname, MODULE_PATH);
+            internalModules = fs.readdirSync(absoluteModulePath)
+                .filter(file => (/^[^\.]+(\.js)?$/).test(file))
+                .reduce((memo, file) => {
+                    var moduleName = file.split(new RegExp('[.\\' + path.sep + ']'))[0];
+                    var modulePath = path.join(absoluteModulePath, file);
+                    if (fs.lstatSync(modulePath).isDirectory()) {
+                        modulePath = path.join(modulePath, "index.js")
+                    }
+                    memo[modulePath] = moduleName;
+                    return memo;
+                }, {});
+        } catch (err) {
+            internalModules = {};
+        }
+        return Object.assign(externalModules.reduce((memo, module) => {
+            try {
+                var modulePath = require.resolve(module);
+                memo[modulePath] = module;
+            } catch (err) {
+                // do something
+            }
+            return memo;
+        }, internalModules));
+    }),
 
-  getBuiltPrebidCoreFile: function(dev) {
-    return path.join(__dirname, dev ? DEV_PATH : BUILD_PATH, 'prebid-core' + '.js');
-  },
+    getBuiltModules: function (dev, externalModules) {
+        var modules = this.getModuleNames(externalModules);
+        if (Array.isArray(externalModules)) {
+            modules = _.intersection(modules, externalModules);
+        }
+        return modules.map(name => path.join(__dirname, dev ? DEV_PATH : BUILD_PATH, name + '.js'));
+    },
 
-  getModulePaths: function(externalModules) {
-    var modules = this.getModules(externalModules);
-    return Object.keys(modules);
-  },
+    getBuiltPrebidCoreFile: function (dev) {
+        return path.join(__dirname, dev ? DEV_PATH : BUILD_PATH, 'prebid-core' + '.js');
+    },
 
-  getModuleNames: function(externalModules) {
-    return _.values(this.getModules(externalModules));
-  },
+    getModulePaths: function (externalModules) {
+        var modules = this.getModules(externalModules);
+        return Object.keys(modules);
+    },
 
-  nameModules: function(externalModules) {
-    var modules = this.getModules(externalModules);
-    return through.obj(function(file, enc, done) {
-      file.named = modules[file.path] ? modules[file.path] : 'prebid';
-      this.push(file);
-      done();
-    })
-  },
+    getModuleNames: function (externalModules) {
+        return _.values(this.getModules(externalModules));
+    },
+
+    nameModules: function (externalModules) {
+        var modules = this.getModules(externalModules);
+        return through.obj(function (file, enc, done) {
+            file.named = modules[file.path] ? modules[file.path] : 'prebid';
+            this.push(file);
+            done();
+        })
+    },
 }
