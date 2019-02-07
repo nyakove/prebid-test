6c00e4996f233616ba4f64a8a2e8539c0bdb8f01
nyakove <nyakove@gmail.com>
2019-02-07 14:01:45 +0200

    added src JS files to repo

---
+++ files.js
@@ -0,0 +1,42 @@
+const _fs = require('fs');
+const express = require('express');
+const app = express();
+
+let adaptersList = async function () {
+    var path = './tstmod';
+    var options = {
+        withFileTypes: true
+    };
+
+    var filesArray = await _fs.promises.readdir(path, options);
+
+
+    var files = [];
+
+    for (file of filesArray) {
+        if (file.name.slice(-13) == 'BidAdapter.js')
+            files.push(file.name.slice(0, -3))
+    }
+
+    await _fs.promises.writeFile('test.json', JSON.stringify(files));
+
+    let json = await _fs.promises.readFile('test.json', 'utf-8');
+    
+    return json;
+
+}
+
+
+app.get('/getList', async function (req, res) {
+    let response = await adaptersList();
+    res.send(response);
+    res.end();
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/files.html')
+});
+
+app.listen(8080, function () {
+    console.dir('Server started at port 8080');
+})
